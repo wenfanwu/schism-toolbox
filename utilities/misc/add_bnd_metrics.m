0cda73279f85885ee588bbeec29aa2eb1d4f4ae8
wenfanwu <48357922+wenfanwu@users.noreply.github.com>
2024-11-11 09:49:45 -0500

    Update add_bnd_metrics.m
    A more rigorous algorithm is used to determine the rotation direction of the land/open boundaries

--- utilities/misc/add_bnd_metrics.m
+++ utilities/misc/add_bnd_metrics.m
@@ -2,48 +2,154 @@ function Mobj = add_bnd_metrics(Mobj, obc_nodes, land_nodes, island_nodes)
 % Add open/land/island boundary info.
 %
 %% Syntax
-%
+% Mobj = add_bnd_metrics(Mobj, obc_nodes, land_nodes, island_nodes)
 %
 %% Description
-%
+% Mobj = add_bnd_metrics(Mobj, obc_nodes, land_nodes, island_nodes)
 %
 %% Examples 
 % 
 %
 %% Input Arguments
-%
+% Mobj - mesh object; datastruct
+%       a datastruct containing the mesh info.
+% obc_nodes - open boundary nodes; double
+%       a matrix (M*N) containing the open boundary info, with M indicating
+%       the node number of the longest open boundary, while N indicating
+%       the # of open boundaries.
+% land_nodes - open boundary nodes; double
+%       a matrix (M*N) containing the land boundary info, with M indicating
+%       the node number of the longest land boundary, while N indicating
+%       the # of land boundaries.
+% islandc_nodes - open boundary nodes; double
+%       a matrix (M*N) containing the island boundary info, with M indicating
+%       the node number of the longest island boundary, while N indicating
+%       the # of island boundaries.
 %
 %% Output Arguments
-%
+% Mobj - mesh object; datastruct
+%       Mobj with updated boundary info.
 %
 %% Notes
 % The outer boundaies (land/sea) should be aligned anti-clockwise while the
 % inner boundaries (island) should be clockwise.
-% 
-% This function was generated with the help of ChatGPT
 %
 %% Author Info
 % Created by Wenfan Wu, Virginia Institute of Marine Science in 2024. 
-% Last Updated on 1 Nov 2024. 
+% Last Updated on 11 Nov 2024. 
 % Email: wwu@vims.edu
 % 
-% See also: 
-
+% See also: add_grid_metrics
 %% Parse inputs
-% 1) the islands don't need to form a loop in hgrid.gr3 or hgrid.ll
+% 1) arrange all boundaries in descending order based on the # of nodes
+% 2) trim redundant lines that are all zeros
+obc_nodes = trim_bnd_nodes(obc_nodes);
+land_nodes = trim_bnd_nodes(land_nodes);
+island_nodes = trim_bnd_nodes(island_nodes);
+
+%% Check the island loops (inner loops)
+% the islands don't need to form a loop in hgrid.gr3 or hgrid.ll
 ind_btm = sub2ind(size(island_nodes), sum(island_nodes~=0, 1), 1:size(island_nodes,2));
 if sum(island_nodes(1,:) ~= island_nodes(ind_btm))==0
     disp('removed ending points of islands, since all islands start/end at the same node')
     island_nodes(ind_btm) = 0;
 end
 
-% 2) arrange all boundaries in descending order based on the # of nodes
-% 3) trim redundant lines that are all zeros
-% 4) ensure directions of all boundary nodes are correct
-obc_nodes = trim_bnd_nodes(Mobj, obc_nodes, 'open');
-land_nodes = trim_bnd_nodes(Mobj, land_nodes, 'land');
-island_nodes = trim_bnd_nodes(Mobj, island_nodes, 'island');
+% make sure each island loop is aligned clockwise
+note_flag = 0;
+island_counts = size(island_nodes, 2);
+for ii = 1:island_counts
+    tmp_nodes = island_nodes(:, ii);
+    ind_valid = tmp_nodes~=0;
 
+    if ispolycw(Mobj.lon(tmp_nodes(ind_valid)), Mobj.lat(tmp_nodes(ind_valid)))==0
+        disp(['the island boundary (#', num2str(ii),') is aligned anti-clockwise and has been adjusted to clockwise'])
+        island_nodes(ind_valid, ii) = flip(tmp_nodes(ind_valid),1);
+        note_flag = 1;
+    end
+end
+
+if note_flag==0
+    disp('all island boundaries are aligned clockwise (great)')
+end
+%% Check the land-sea loop (outer loop)
+% make sure land/open boundary are aligned anti-clockwise
+obc_counts = size(obc_nodes,2);
+land_counts = size(land_nodes,2);
+
+outer_bnd_cells = cell(obc_counts+land_counts,1);
+tmp_nodes = obc_nodes(:,1);
+tmp_nodes(tmp_nodes==0) = [];
+outer_bnd_cells{1} = tmp_nodes(:);
+ind_flags = ones(obc_counts+land_counts,1);
+for ii = 2:obc_counts+land_counts
+    head_nodes = outer_bnd_cells{ii-1};
+    switch mod(ii,2)
+        case 0
+            next_nodes = land_nodes;
+        case 1
+            next_nodes = obc_nodes;
+    end
+    [idx1, idx2] = find(next_nodes==head_nodes(end));
+    tmp_nodes = next_nodes(:,idx2); 
+    tmp_nodes(tmp_nodes==0) = []; tmp_nodes = tmp_nodes(:);
+    if idx1~=1
+        tmp_nodes = flip(tmp_nodes,1);
+        ind_flags(ii) = -1;
+    end
+    outer_bnd_cells{ii} = tmp_nodes(:);
+end
+outer_loop = unique(cell2mat(outer_bnd_cells), 'stable');
+
+% check the land-sea loop direction
+if ispolycw(Mobj.lon(outer_loop), Mobj.lat(outer_loop)) == 1  % if the land-sea loop is aligned clockwise
+    outer_bnd_cells = cellfun(@(x) flip(x,1), outer_bnd_cells, 'UniformOutput',false);
+    ind_flags = ind_flags*-1;
+end
+
+% arrange the land/open boundary nodes in descending order
+obc_flags = ind_flags(1:2:end);
+obc_cells = outer_bnd_cells(1:2:end);
+
+obc_lens = cellfun(@(x) length(x), obc_cells);
+[~, ind_sorted] = sort(obc_lens, 'descend');
+obc_cells = obc_cells(ind_sorted); % sort by the obc  lens
+obc_flags = obc_flags(ind_sorted);
+
+land_flags = ind_flags(2:2:end);
+land_cells = outer_bnd_cells(2:2:end);
+land_lens = cellfun(@(x) length(x), land_cells);
+[~, ind_sorted] = sort(land_lens, 'descend');
+land_cells = land_cells(ind_sorted); % sort by the land lens
+land_flags = land_flags(ind_sorted);
+
+note_flag = 0;
+obc_nodes = zeros(max(obc_lens), obc_counts);
+for ii = 1:obc_counts
+    tmp_nodes = obc_cells{ii};
+    obc_nodes(1:numel(tmp_nodes), ii) = tmp_nodes;
+    if obc_flags(ii)==-1
+        disp(['the open boundary (#', num2str(ii),') is aligned clockwise and has been adjusted to anti-clockwise'])
+        note_flag = 1;
+    end
+end
+if note_flag==0
+    disp('all open boundaries are aligned anti-clockwise (great)')
+end
+
+note_flag = 0;
+land_nodes = zeros(max(land_lens), land_counts);
+for ii = 1:land_counts
+    tmp_nodes = land_cells{ii};
+    land_nodes(1:numel(tmp_nodes), ii) = tmp_nodes;
+    if land_flags(ii)==-1
+        disp(['the land boundary (#', num2str(ii),') is aligned clockwise and has been adjusted to anti-clockwise'])
+        note_flag = 1;
+    end
+end
+if note_flag==0
+    disp('all land boundaries are aligned anti-clockwise (great)')
+end
 %% Add boundary info
 %========= open boundary=========
 Mobj.obc_nodes = obc_nodes;
@@ -56,7 +162,6 @@ Mobj.obc_nodes_tot(Mobj.obc_nodes_tot==0) = [];
 Mobj.land_nodes = land_nodes;
 Mobj.land_counts = size(Mobj.land_nodes,2);
 Mobj.land_lens = sum(Mobj.land_nodes~=0)';
-% Mobj.nNodes_land = sum(Mobj.land_lens);
 Mobj.land_nodes_tot = Mobj.land_nodes(:);
 Mobj.land_nodes_tot(Mobj.land_nodes_tot==0) = [];
 
@@ -64,18 +169,15 @@ Mobj.land_nodes_tot(Mobj.land_nodes_tot==0) = [];
 Mobj.island_nodes = island_nodes;
 Mobj.island_counts = size(Mobj.island_nodes,2);
 Mobj.island_lens = sum(Mobj.island_nodes~=0)';
-% Mobj.nNodes_island = sum(Mobj.island_lens);
 Mobj.island_nodes_tot = Mobj.island_nodes(:);
 Mobj.island_nodes_tot(Mobj.island_nodes_tot==0) = [];
 
-disp('land/island/open boundary metrics have been added')
+disp('open/land/island boundary metrics have been added')
 end
 
-function bnd_nodes = trim_bnd_nodes(Mobj, bnd_nodes, bnd_str)
-% This function has three goals:
+function bnd_nodes = trim_bnd_nodes(bnd_nodes)
 % 1) arrange all boundaries in descending order based on the # of nodes
 % 2) trim redundant lines that are all zeros
-% 3) ensure directions of all boundary nodes are correct
 
 max_len = max(sum(bnd_nodes~=0,1));
 bnd_nodes = bnd_nodes(1:max_len,:);
@@ -84,48 +186,15 @@ bnd_lens = sum(bnd_nodes~=0,1);
 [~, ind_sorted] = sort(bnd_lens, 'descend');
 bnd_nodes = bnd_nodes(:, ind_sorted); % sort by the # of loop lens
 
-note_flag = 0;
-bnd_counts = size(bnd_nodes, 2);
-for ii = 1:bnd_counts
-    bnd_tmp = bnd_nodes(:, ii);
-    ind_valid = bnd_tmp~=0;
-    if strcmp(bnd_str, 'island')
-        cw_flag = 0; old_str = 'anti-clockwise'; new_str = 'clockwise';
-    else
-        cw_flag = 1; old_str = 'clockwise'; new_str = 'anti-clockwise';
-    end
-    if ispolycw(Mobj.lon(bnd_tmp(ind_valid)), Mobj.lat(bnd_tmp(ind_valid)))==cw_flag
-        disp(['the ',bnd_str,' boundary (#', num2str(ii),') is aligned ', old_str,' and has been adjusted to ', new_str])
-        bnd_nodes(ind_valid, ii) = flip(bnd_tmp(ind_valid),1);
-        note_flag = 1;
-    end
 end
 
-if note_flag==0
-    disp(['all ', bnd_str, ' boundaries are aligned ', new_str, ' (great)'])
-end
-
-% debug
-% sz = rescale(1:numel(bnd_tmp(ind_valid)))*20+1;
-% scatter(Mobj.lon(bnd_tmp(ind_valid)), Mobj.lat(bnd_tmp(ind_valid)), sz)
-
-end
-
-% function Mobj = get_obc_elems(Mobj)
-% % calculate the index of open boundary nodes.
-% 
-% obc_nodes = Mobj.obc_nodes;
-% Mobj.obc_elems = zeros(size(obc_nodes));
-% nBnds = size(obc_nodes,2);
-% for ibnd = 1:nBnds
-%     indVert1 = Mobj.tri(:,1)==obc_nodes(:,ibnd)';
-%     indVert2 = Mobj.tri(:,2)==obc_nodes(:,ibnd)';
-%     indVert3 = Mobj.tri(:,3)==obc_nodes(:,ibnd)';
-%     indVert = indVert1+indVert2+indVert3;
-%     tmp = sum(indVert,2);
-%     tmp = find(tmp>1);
-%     Mobj.obc_elems(1:length(tmp),ibnd) = tmp;
-% end
-% 
-% Mobj.obc_elems(sum(Mobj.obc_elems, 2)==0, :) = [];
-% end
+%% Debug (check the direction of boundaries)
+% tmp_nodes = Mobj.land_nodes(:,1);
+% tmp_nodes(tmp_nodes==0) = [];
+% figure
+% sz = rescale(1:numel(tmp_nodes))*60+1;
+% scatter(Mobj.lon(tmp_nodes), Mobj.lat(tmp_nodes), sz)
+% hold on
+% scatter(Mobj.lon(tmp_nodes(1)), Mobj.lat(tmp_nodes(1)), 20, 'red')
+% plot(Mobj.lon(tmp_nodes), Mobj.lat(tmp_nodes))
+% plot_schism_bnds(Mobj)
