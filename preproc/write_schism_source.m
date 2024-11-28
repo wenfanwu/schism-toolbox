ae003b9723423caab448741cdf426cc39f338591
wenfanwu <48357922+wenfanwu@users.noreply.github.com>
2024-11-27 20:43:56 -0500

    minor changes
    delete write_schism_elev_th, which can be replaced by write_schism_th
    improve write_schism_source

--- preproc/write_schism_source.m
+++ preproc/write_schism_source.m
@@ -1,13 +1,8 @@
 function write_schism_source(Mobj, D, tracer_list)
-% the # of sink/source can not be zeros in SCHISM, but you can set the
-% flow volumn to be zero to if the sink/source point is not needed. In
-% addition, the source/sink values must be single-precision.
-% Source/sinks can be specified at an elem more than once, and the code
-% will accumulate the volumes, hence if you assign multiple elems for one
-% river, make sure that the sum of the volumes is equal to the runoff.
-% 
+% Write source_sink.in and relevant files for SCHISM
+%
 %% Syntax
-% 
+% write_schism_source(Mobj, D, tracer_list)
 %
 %% Description 
 % 
@@ -22,27 +17,33 @@ function write_schism_source(Mobj, D, tracer_list)
 % 
 % 
 %% Notes
-%
+% the # of sink/source can not be zeros in SCHISM, but you can set the
+% flow volumn to be zero to if the sink/source point is not needed. In
+% addition, the source/sink values must be single-precision.
+% Source/sinks can be specified at an elem more than once, and the code
+% will accumulate the volumes, hence if you assign multiple elems for one
+% river, make sure that the sum of the volumes is equal to the runoff.
 %
 %% Author Info
-% Created by Wenfan Wu, Ocean Univ. of China in 2023. 
-% Last Updated on 2023-11-26.
-% Email: wenfanwu@stu.ouc.edu.cn
+% Created by Wenfan Wu, Virginia Institute of Marine Science in 2021.
+% Last Updated on 27 Nov 2024
+% Email: wwu@vims.edu
 % 
-% See also: 
+% See also: write_schism_source_nc
 
 %% Parse inputs
 if nargin < 3
     tracer_list = {'temp', 'salt'}; 
 end
-nsk = numel(D.source_elems);
-nsr = numel(D.sink_elems);
-if nsk == 0
+
+n_src = numel(D.source_elems);
+n_snk = numel(D.sink_elems);
+if n_src == 0
     D.vsource.flow = zeros(size(D.vsource.time));
     D.msource.temp = zeros(size(D.vsource.time));
     D.msource.salt = zeros(size(D.vsource.time));
 end
-if nsr == 0
+if n_snk == 0
     D.vsink.flow = zeros(size(D.vsink.time));
     D.sink_elems = 1;
 end
@@ -50,13 +51,13 @@ end
 %% source_sink.in
 fileName = [Mobj.aimpath, 'source_sink.in']; %source_sink.in; specify the source/sink elements
 fid = fopen(fileName,'wt');
-fprintf(fid, [num2str(max(1,nsk)), '\n']);
-for ii = 1:max(1,nsk)
+fprintf(fid, [num2str(max(1,n_src)), '\n']);
+for ii = 1:max(1,n_src)
     fprintf(fid, [num2str(D.source_elems(ii)), '\n']);
 end
 fprintf(fid, '         \n');  % this blank line is required!
-fprintf(fid, [num2str(max(1,nsr)), '\n']);
-for ii = 1:max(1,nsr)
+fprintf(fid, [num2str(max(1,n_snk)), '\n']);
+for ii = 1:max(1,n_snk)
     fprintf(fid, [num2str(D.sink_elems(ii)), '\n']);
 end
 fclose(fid);
@@ -71,8 +72,7 @@ nTimes = length(D.msource.time);
 varAll = [];
 for ii = 1:nTracers
     tracer_name = tracer_list{ii};
-%     eval(['varTmp = D.msource.',tracer_name,';'])
-    varTmp = D.msource(tracer_name);
+    varTmp = D.msource.(tracer_name);
     if size(varTmp,1) ~= nTimes
         varTmp = varTmp';
     end
